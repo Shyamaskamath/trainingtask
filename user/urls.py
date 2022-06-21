b07dc9a8c5c063532b84ec8fd5c29e704508587e
shyamakamath <shyama.nuventure.in>
2022-06-21 23:56:07 +0530

    homepage for adminuser

--- user/urls.py
+++ user/urls.py
@@ -3,8 +3,6 @@ from .import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('register/',views.RegisterationFormView.as_view(),name="signup"),
-    path('home/',views.HomePageView.as_view(),name="home"),
-    path('login/',views.LoginView.as_view(),name="login"),
+    path('',views.LoginView.as_view(),name="login"),
     path('logout/',auth_views.LogoutView.as_view(next_page="login"), name='logout'),
 ] 
