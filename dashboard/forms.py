1e691f1b49c303eb6ba2c35d2adb3ec954958913
shyamakamath <shyama.nuventure.in>
2022-06-22 10:26:18 +0530

    renamed the usercreationform to NewUserCreationForm in forms.py

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -9,7 +9,7 @@ def validateemail(value):
     if User.objects.filter(email = value).exists():
         raise ValidationError((f"{value} is taken!"),params = {'value':value})
 
-class NewUserCReationForm(UserCreationForm):
+class NewUserCreationForm(UserCreationForm):
     """User Creation form"""
     email = forms.EmailField(validators = [validateemail])
     is_staff = forms.BooleanField(required=False)
