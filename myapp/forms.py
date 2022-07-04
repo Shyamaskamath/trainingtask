3eab0bbe5ca708e659713b86bb6d35d2db16fff4
shyamakamath <shyama.nuventure.in>
2022-07-04 07:57:41 +0530

    modified updateview to delete the exisiting image

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,6 +1,7 @@
 from django import forms 
 from .models import Product,ProductImage
 from django.forms import modelformset_factory
+from django.core.exceptions import ValidationError
 
 class ProductForm(forms.ModelForm):
     """form to create new products"""
@@ -10,6 +11,7 @@ class ProductForm(forms.ModelForm):
 
 class ProductImageForm(forms.ModelForm):
     """form to add product images"""
+    
     class Meta:
         model = ProductImage
         fields = ('image',)
@@ -20,5 +22,5 @@ class ProductEditForm(forms.ModelForm):
         model = Product
         fields = '__all__'
 
-"""formset  to store 3 image per product """
+"""formset  to get 3 image per product """
 ImageFormSet = modelformset_factory(ProductImage, form=ProductImageForm, extra=3,max_num=3,min_num=0)
--- myapp/views.py
+++ myapp/views.py
@@ -1,16 +1,14 @@
 from django.shortcuts import redirect, render
-from .forms import ProductForm, ProductImageForm, ProductEditForm,ImageFormSet
+from .forms import ProductForm,ProductEditForm, ImageFormSet
 from .models import ProductImage, Product
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponseRedirect
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import DeleteView,FormView,UpdateView
-from django.urls import reverse_lazy,reverse
-from django.contrib.auth.mixins import LoginRequiredMixin,AccessMixin
+from django.views.generic.edit import DeleteView, UpdateView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin, AccessMixin
 from django.views.generic import TemplateView, CreateView
 from user.models import CustomUser
-from django.contrib.auth.decorators import login_required
-from django.contrib.admin.views.decorators import staff_member_required
 from django.conf import settings
 from django.contrib import messages
 from django.views import View
@@ -19,7 +17,7 @@ from django.views import View
 class StaffRequiredMixin(AccessMixin):
     """
     View mixin which checks user is a staff member"""
-    
+
     def dispatch(self, request, *args, **kwargs):
         if not request.user.is_staff:
             messages.error(
@@ -28,44 +26,49 @@ class StaffRequiredMixin(AccessMixin):
                 'requested operation.')
             return redirect(settings.LOGIN_URL)
         return super(StaffRequiredMixin, self).dispatch(request,
-            *args, **kwargs)
+                                                        *args, **kwargs)
+
 
 class HomePageView(LoginRequiredMixin, TemplateView):
     """ view for homepage"""
     template_name = 'myapp/homepage.html'
+  
 
-
-class PeoplePageView(LoginRequiredMixin,StaffRequiredMixin,ListView):
+class PeoplePageView(LoginRequiredMixin, StaffRequiredMixin, ListView):
     """views for getting list adminusers and customerusers """
     template_name = "myapp/people.html"
     model = CustomUser
+
     def get_context_data(self, **kwargs):
         objectlist = super(PeoplePageView, self).get_context_data(**kwargs)
-        objectlist['admin'] = CustomUser.objects.filter(groups__name='adminusers')
-        objectlist['customer'] = CustomUser.objects.filter(groups__name='customer')
+        objectlist['admin'] = CustomUser.objects.filter(
+            groups__name='adminusers')
+        objectlist['customer'] = CustomUser.objects.filter(
+            groups__name='customer')
         return objectlist
 
 class ProductListView(LoginRequiredMixin, ListView):
-    pass
     """ view for rendering all the products """
     model = Product
     context_object_name = 'product'
     template_name = 'myapp/productlist.html'
 
-class DeleteProductView(LoginRequiredMixin,StaffRequiredMixin, DeleteView):
+class DeleteProductView(LoginRequiredMixin, StaffRequiredMixin, DeleteView):
     """view to delete a product"""
     model = Product
     context_object_name = "product"
     template_name = "myapp/deleteproduct.html"
     success_url = reverse_lazy('productlist')
-    
+
+
 class DetailProductView(LoginRequiredMixin, DetailView):
     """ view for displaying detailed view of a product"""
     context_object_name = "product"
     model = Product
     template_name = "myapp/detail.html"
 
-class AddProductView(LoginRequiredMixin,StaffRequiredMixin,CreateView):
+
+class AddProductView(LoginRequiredMixin, StaffRequiredMixin, CreateView):
     """view to create new product"""
     model = Product
     template_name = 'myapp/productcreate.html'
@@ -76,8 +79,8 @@ class AddProductView(LoginRequiredMixin,StaffRequiredMixin,CreateView):
         productform = ProductForm()
         formset = ImageFormSet(queryset=ProductImage.objects.none())
         return render(request, 'myapp/productcreate.html', {"productform": productform, "formset": formset})
-    
-    def post(self, request, *args, **kwargs): 
+
+    def post(self, request, *args, **kwargs):
         productform = ProductForm(request.POST)
         formset = ImageFormSet(request.POST, request.FILES)
         if productform.is_valid() and formset.is_valid():
@@ -88,9 +91,9 @@ class AddProductView(LoginRequiredMixin,StaffRequiredMixin,CreateView):
                     ProductImage.objects.create(image=image, product=p)
             return redirect("productlist")
         else:
-            print(productform.errors, formset.errors)   
+            print(productform.errors, formset.errors)
 
-class ProductEditView(LoginRequiredMixin,StaffRequiredMixin,UpdateView):
+class ProductEditView(LoginRequiredMixin, StaffRequiredMixin, UpdateView):
     """view to update product details"""
     template_name = "myapp/edit.html"
     model = Product
@@ -99,27 +102,32 @@ class ProductEditView(LoginRequiredMixin,StaffRequiredMixin,UpdateView):
 
     def get(self, request, *args, **kwargs):
         self.object = self.get_object()
-        print(self.object.id)
         form = ProductEditForm(instance=self.object)
-        formset =  ImageFormSet(queryset=ProductImage.objects.filter(product= self.object))
+        formset = ImageFormSet(
+            queryset=ProductImage.objects.filter(product=self.object))
         context = {
             'form': form,
-            'formset':formset,
+            'formset': formset,
         }
-        
-        return render(request,'myapp/edit.html',context)
 
-    def post(self, request, *args, **kwargs): 
+        return render(request, 'myapp/edit.html', context)
+
+    def post(self, request, *args, **kwargs):
         product = self.get_object()
-        form = ProductEditForm(request.POST,instance=product)
-        formset =  ImageFormSet(request.POST or None, request.FILES or None)
+        form = ProductEditForm(request.POST, instance=product)
+        formset = ImageFormSet(request.POST or None, request.FILES or None)
         if form.is_valid() and formset.is_valid():
-            form.save() 
+            form.save()
             for image in formset.forms:
+                # for adding newimage or updating images
                 if image.is_valid():
                     if image.cleaned_data.get('image'):
                         image.instance.product = product
                         image.instance.save()
-                        image.save()   
-        return HttpResponseRedirect(product.get_absolute_url()) 
-            
+                        image.save()
+                     # to delete the exisiting image
+                    elif image.cleaned_data.get('image') is False:
+                        photo = image.cleaned_data['id']
+                        photo.delete()
+
+        return HttpResponseRedirect(product.get_absolute_url())
